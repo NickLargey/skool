2fb2e8f194e60f1b3dd959bc25707fca75e58873
___GNUk <Nick.Largey@gmail.com>
2024-02-26 18:03:18 -0500

    final model (still bad)

--- BigramModel.py
+++ BigramModel.py
@@ -14,8 +14,9 @@ from nltk.corpus import stopwords
 pp = pprint.PrettyPrinter()
 stop_words = set(stopwords.words('english'))
 
-punctuation_except_apostrophe = string.punctuation.replace("'", "")
-pattern = f"[{re.escape(punctuation_except_apostrophe)}]"
+
+punct_pattern = re.escape(string.punctuation)
+split_casing = r'([a-z])([A-Z])'
 
 def read_files_in_directory(directory_path):
   dic_term_frequency = {}
@@ -25,7 +26,7 @@ def read_files_in_directory(directory_path):
       for line in rfile:
         current_line = line.strip()
         current_line = current_line.lower()
-        current_line = re.sub(pattern, '', current_line)
+        current_line = re.sub(punct_pattern, '', current_line)
         tokens = word_tokenize(current_line)          
         filtered_sentence = [w for w in tokens if not w in stop_words]
 
@@ -57,37 +58,40 @@ def freq_to_prob(dic_term_frequency, dic_pairs_frequency):
 
 
 
-def calculate_probability(dic_term_prob, input_text, mode='uni'):
+def calculate_probability(dic_term_prob, input_text):
     prob = 0.0
-    if mode == 'uni':
-      s_text = word_tokenize(input_text)
-      filtered_sentence = [w.lower() for w in s_text if not w.lower() in stop_words]
-    if mode == 'bi':
-      s_text = word_tokenize(input_text)
-      filtered_sentence = [w.lower() for w in s_text if not w.lower() in stop_words]
-      filtered_sentence = [(filtered_sentence[i], filtered_sentence[i+1]) for i in range(len(filtered_sentence) - 1)]
-    else:
-      raise ValueError('Invalid mode')
-    
+    s_text = word_tokenize(input_text)
+    text = re.sub('\s', ' ', input_text)
+    text = re.sub(split_casing, r'\1 \2', text)
+    text = text.strip()
+    text = text.lower()
+    s_text = re.sub(f'[{punct_pattern}]', '', text)
+    s_text = word_tokenize(s_text)
+
+    filtered_sentence = [w.lower() for w in s_text if not w.lower() in stop_words]
+    filtered_sentence = [(filtered_sentence[i], filtered_sentence[i+1]) for i in range(len(filtered_sentence) - 1)]
+
     for word in filtered_sentence:
         if word in dic_term_prob.keys():
-            prob += math.log10(dic_term_prob[word])
-            print(f'Found! Word: {word} Genre:{dic_term_prob[word]} Prob: {prob}')
+            prob += math.log(dic_term_prob[word])
         else:
-            n = 1/math.log10(len(dic_term_prob))
-            prob += n 
-            print(f'Not Found :( Word: {word} N: {n} Prob: {prob}')      
+            n = 1/math.log(len(dic_term_prob))
+            prob += n   
     return prob
 
-def run_model(p_dict, df, m):
-   for i in range(len(df)):
+def run_model(p_dict, df, genres):
+    res_df = pd.DataFrame(columns=["Predicted"])
+    for i in range(len(df)):
         prob_dict = {}
         for genre in p_dict:
-            p = calculate_probability(p_dict[genre], df["Text"].iloc[i], mode=m)
+            p = calculate_probability(p_dict[genre], df["Text"].iloc[i])
             prob_dict[genre] = p 
-        
+
         max_genre = max(prob_dict, key=prob_dict.get) # Find the genre with the maximum probability
         max_value = prob_dict[max_genre]
+        
+        pred = pd.DataFrame({"Predicted": [max_genre]})
+        res_df = pd.concat([res_df, pred], ignore_index=True)
 
         # Print the maximum value and its corresponding true value from the DataFrame
         print(f'Song # {i + 1}')    
@@ -99,23 +103,64 @@ def run_model(p_dict, df, m):
         pp.pprint(sorted_dict)
         print('\n') 
 
+    best_res_df = pd.concat(
+            [df, res_df], axis=1)
+    confusion_matrix(best_res_df, genres)
+    f1_scores, f1 = f1_score(best_res_df, genres)
+    print(f'F1 Scores: {f1_scores}')
+    print(f'Average F1 Score: {f1}')
+
+def confusion_matrix(c_list, genres):
+    c_matrix = pd.DataFrame(0, index=genres, columns=genres)
+
+    for i in range(len(c_list)):
+        c_matrix.loc[c_list['Genre'].iloc[i], c_list['Predicted'].iloc[i]] += 1
+
+    print(c_matrix, '\n')
+
+# Precision = instances correctly classified for a class/all instances both true and false called out by the model
+# Recall = instances correctly classified for a class/all instances that truly belong to the class
+def f1_score(df, genres):
+    genres = genres
+    f1_scores = {}
+    macro_f1 = 0
+    for genre in genres:
+        # True positives: Predicted = genre and Actual = genre
+        tp = len(df[(df['Predicted'] == genre) & (df['Genre'] == genre)])
+        # False positives: Predicted = genre but Actual != genre
+        fp = len(df[(df['Predicted'] == genre) & (df['Genre'] != genre)])
+        # False negatives: Predicted != genre but Actual = genre
+        fn = len(df[(df['Predicted'] != genre) & (df['Genre'] == genre)])
+        # Precision and Recall
+        precision = tp / (tp + fp) if (tp + fp) != 0 else 0
+        recall = tp / (tp + fn) if (tp + fn) != 0 else 0
+        # F1 Score
+        f1 = 2 * (precision * recall) / (precision +
+                                         recall) if (precision + recall) != 0 else 0
+
+        f1_scores[genre] = f1
+        macro_f1 += f1
+    avg_f1 = macro_f1 / len(genres)    
+    return f1_scores, avg_f1
+
+
 def main():
     freq_dict_uni  = {}
     freq_dict_bi = {}
     uni_prob = {}
     bi_prob = {}
-
+    genres = []
     for root, dirs, files in os.walk('./TM_CA1_Lyrics/'):
         for dir in dirs:
+            genres.append(str(dir))
             freq_dict_uni[dir], freq_dict_bi[dir] = read_files_in_directory(root + dir + '/')
             uni_prob[dir], bi_prob[dir] = freq_to_prob(freq_dict_uni[dir], freq_dict_bi[dir])
-    pp.pprint(uni_prob)
-    pp.pprint(bi_prob)
-    # df = pd.read_csv("./test.tsv", sep='\t')
-   
-    # run_model(uni_prob, df, mode='uni')
-    # run_model(bi_prob, df, 'bi') 
+
+    df = pd.read_csv("./test.tsv", sep='\t')
+
+    run_model(bi_prob, df, genres) 
     
+
 
 if __name__ == '__main__':
     main()
--- MixedModel.py
+++ MixedModel.py
@@ -13,9 +13,7 @@ from nltk.corpus import stopwords
 
 pp = pprint.PrettyPrinter()
 stop_words = set(stopwords.words('english'))
-
-punctuation_except_apostrophe = string.punctuation.replace("'", "")
-pattern = f"[{re.escape(punctuation_except_apostrophe)}]"
+punct_pattern = re.escape(string.punctuation)
 split_casing = r'([a-z])([A-Z])'
 
 
@@ -31,7 +29,7 @@ def read_files_in_directory(directory_path):
                 for line in rfile:
                     current_line = line.strip()
                     current_line = current_line.lower()
-                    current_line = re.sub(pattern, '', current_line)
+                    current_line = re.sub(f'[{punct_pattern}]', '', current_line)
                     tokens = word_tokenize(current_line)
                     filtered_sentence = [
                         w for w in tokens if not w in stop_words]
@@ -56,7 +54,7 @@ def read_files_in_directory(directory_path):
                 text = re.sub('\s', ' ', r_text)
                 text = text.strip()
                 text = text.lower()
-                text = re.sub(pattern, '', text)
+                text = re.sub(f'[{punct_pattern}]', '', text)
                 tokens = word_tokenize(text)
                 filtered_sentence = [
                     w for w in tokens if not w in stop_words]
@@ -77,12 +75,17 @@ def freq_to_prob(dic_term_frequency, dic_pairs_frequency):
     total_pairs = sum(dic_pairs_frequency.values()) + len(dic_pairs_frequency)
     dic_pairs_prob = {pair: count / total_pairs for pair,
                       count in dic_pairs_frequency.items()}
-
     return dic_term_prob, dic_pairs_prob
 
 
 def calculate_probability(dic_term_prob, input_text, mode):
     prob = 0.0
+    text = re.sub('\s', ' ', input_text)
+    text = re.sub(split_casing, r'\1 \2', text)
+    text = text.strip()
+    text = text.lower()
+    s_text = re.sub(f'[{punct_pattern}]', '', text)
+                
     if mode == 'uni':
         s_text = word_tokenize(input_text)
         filtered_sentence = [w.lower()
@@ -100,9 +103,8 @@ def calculate_probability(dic_term_prob, input_text, mode):
         if word in dic_term_prob.keys():
             prob += math.log(dic_term_prob[word])
         else:
-            n = 1/math.log(len(dic_term_prob))
-            prob += n
-    return prob
+            prob += 1/math.log(len(dic_term_prob))
+    return math.exp(prob)
 
 
 def mixed_model(u_prob_dict, b_prob_dict, test_df, genres):
@@ -127,15 +129,10 @@ def mixed_model(u_prob_dict, b_prob_dict, test_df, genres):
                 prob_dict[genre] = p
 
             max_genre = max(prob_dict, key=prob_dict.get)
-            max_value = prob_dict[max_genre]
             true_genre = test_df['Genre'].iloc[i]
 
             if max_genre == true_genre:
                 curr_tp_cnt += 1
-            # print(f'Song # {i + 1}')
-            # print(
-            #     f"Max Probability Genre: {max_genre} with probability {max_value}")
-            # print(f"True Genre: {test_df['Genre'].iloc[i]}")
             res_df = pd.concat([res_df, pd.DataFrame(
                 {'Predicted': [max_genre], 'True': [true_genre]})], ignore_index=True)
 
@@ -144,14 +141,15 @@ def mixed_model(u_prob_dict, b_prob_dict, test_df, genres):
             best_lambda = greek_lambda
             best_res_df = pd.concat(
                 [test_df, pd.DataFrame(res_df['Predicted'])], axis=1)
-        # print(
-        #     f'Best TP Count: {best_tp_cnt} with Lambda: {best_lambda} with best res list: {best_res_df} \n')
-        print(greek_lambda, curr_tp_cnt, best_tp_cnt, best_lambda)
+        
+        # print(f'Lambda: {greek_lambda:.3f}, Current Best Lambda {best_lambda:.3f}, Current True Positive Count: {curr_tp_cnt}, Best True Positive Count: {best_tp_cnt}')
         greek_lambda -= 0.01
-    print(best_res_df)
+    
     print(f'Lambda: {best_lambda}')
     confusion_matrix(best_res_df, genres)
-    f1_score(best_res_df)
+    f1_scores, f1 = f1_score(best_res_df, genres)
+    print(f'F1 Scores: {f1_scores}')
+    print(f'Average F1 Score: {f1}')
 
 
 def confusion_matrix(c_list, genres):
@@ -164,18 +162,17 @@ def confusion_matrix(c_list, genres):
 
 # Precision = instances correctly classified for a class/all instances both true and false called out by the model
 # Recall = instances correctly classified for a class/all instances that truly belong to the class
-
-
-def f1_score(df):
-    classes = df['Genre'].unique()
+def f1_score(df, genres):
+    genres = genres
     f1_scores = {}
-    for cls in classes:
-        # True positives: Predicted = cls and Actual = cls
-        tp = len(df[(df['Predicted'] == cls) & (df['Genre'] == cls)])
-        # False positives: Predicted = cls but Actual != cls
-        fp = len(df[(df['Predicted'] == cls) & (df['Genre'] != cls)])
-        # False negatives: Predicted != cls but Actual = cls
-        fn = len(df[(df['Predicted'] != cls) & (df['Genre'] == cls)])
+    macro_f1 = 0
+    for genre in genres:
+        # True positives: Predicted = genre and Actual = genre
+        tp = len(df[(df['Predicted'] == genre) & (df['Genre'] == genre)])
+        # False positives: Predicted = genre but Actual != genre
+        fp = len(df[(df['Predicted'] == genre) & (df['Genre'] != genre)])
+        # False negatives: Predicted != genre but Actual = genre
+        fn = len(df[(df['Predicted'] != genre) & (df['Genre'] == genre)])
         # Precision and Recall
         precision = tp / (tp + fp) if (tp + fp) != 0 else 0
         recall = tp / (tp + fn) if (tp + fn) != 0 else 0
@@ -183,9 +180,10 @@ def f1_score(df):
         f1 = 2 * (precision * recall) / (precision +
                                          recall) if (precision + recall) != 0 else 0
 
-        f1_scores[cls] = f1
-    print(f1_scores)
-    return f1_scores
+        f1_scores[genre] = f1
+        macro_f1 += f1
+    avg_f1 = macro_f1 / len(genres)    
+    return f1_scores, avg_f1
 
 
 def build_train_val_sets(root_path='./TM_CA1_Lyrics/'):
@@ -212,12 +210,9 @@ def build_train_val_sets(root_path='./TM_CA1_Lyrics/'):
 
 
 def main():
-
     uni_prob, bi_prob, validation_df, genres = build_train_val_sets(
         './TM_CA1_Lyrics/')
-
     df = pd.read_csv("./test.tsv", sep='\t')
-
     mixed_model(uni_prob, bi_prob, validation_df, genres)
 
 
